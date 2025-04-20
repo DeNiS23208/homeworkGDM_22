60a1ada2b9ffb5004149054011561945720b983f
Denis <den23208@mail.ru>
2025-04-21 04:40:03 +0500

    start and create models and use migrate

--- config/settings.py
+++ config/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'catalog_db',
+        'USER': 'postgres',
+        'PASSWORD': 232,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
