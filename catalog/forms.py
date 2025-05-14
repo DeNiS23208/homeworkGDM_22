b5e16135bf1fd3b9a3f32708809a9e03ba79a392
Denis <den23208@mail.ru>
2025-05-14 15:59:52 +0800

    ready homework

---
+++ catalog/forms.py
@@ -0,0 +1,47 @@
+# catalog/forms.py
+
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import Product
+
+FORBIDDEN_WORDS = [
+    'казино',
+    'криптовалюта',
+    'крипта',
+    'биржа',
+    'дешево',
+    'бесплатно',
+    'обман',
+    'полиция',
+    'радар',
+]
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name', '')
+        for word in FORBIDDEN_WORDS:
+            if word.lower() in name.lower():
+                raise ValidationError(f'Название содержит запрещённое слово: "{word}"')
+        return name
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description', '')
+        for word in FORBIDDEN_WORDS:
+            if word.lower() in description.lower():
+                raise ValidationError(f'Описание содержит запрещённое слово: "{word}"')
+        return description
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price is not None and price < 0:
+            raise ValidationError('Цена не может быть отрицательной')
+        return price
